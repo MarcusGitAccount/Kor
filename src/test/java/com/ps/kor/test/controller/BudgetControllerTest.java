ed6db479a471413fa90ba338416c5b5b951aa3c4
Marcus Pop <marcuspopb@gmail.com>
2020-03-22 01:31:48 +0200

    Changed to Junit 4.13 and got the test to perform a simple post request.

--- src/test/java/com/ps/kor/test/controller/BudgetControllerTest.java
+++ src/test/java/com/ps/kor/test/controller/BudgetControllerTest.java
@@ -9,24 +9,18 @@ import com.ps.kor.repo.DailyBudgetRepo;
 import com.ps.kor.repo.UserRepo;
 import lombok.extern.log4j.Log4j2;
 import org.junit.Assert;
-import org.junit.Before;
-import org.junit.jupiter.api.BeforeAll;
-import org.junit.jupiter.api.Test;
-import org.junit.jupiter.api.TestInstance;
-import org.junit.runner.Request;
+import org.junit.*;
 import org.junit.runner.RunWith;
-import org.mockito.InjectMocks;
-import org.mockito.Mock;
 import org.mockito.Mockito;
 import org.mockito.MockitoAnnotations;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
-import org.springframework.mock.http.client.MockClientHttpRequest;
 import org.springframework.mock.web.MockHttpServletResponse;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
-import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.MvcResult;
 import org.springframework.test.web.servlet.RequestBuilder;
@@ -38,21 +32,22 @@ import java.util.UUID;
 
 import static org.mockito.Mockito.*;
 
+@Log4j2
 @RunWith(SpringJUnit4ClassRunner.class)
 @WebMvcTest(value = BudgetController.class)
-@Log4j2
+@AutoConfigureMockMvc(addFilters = false)
 public class BudgetControllerTest {
 
   @Autowired
   private MockMvc mockMvc;
 
-  @Mock
+  @MockBean
   private UserRepo userRepo;
 
-  @Mock
+  @MockBean
   private DailyBudgetRepo dailyBudgetRepo;
 
-  @Mock
+  @MockBean
   private BudgetRoleRepo budgetRoleRepo;
 
   private List<BudgetRole> createdRoles;
@@ -60,7 +55,12 @@ public class BudgetControllerTest {
   private List<User> testUsers;
 
   @Before
-  public void setupMocks() {
+  public void setupTest() {
+    MockitoAnnotations.initMocks(this);
+
+    createdRoles = new LinkedList<>();
+    testUsers = new LinkedList<>();
+
     // When a role is created during a test, save it.
     when(budgetRoleRepo.save(Mockito.any(BudgetRole.class)))
         .then(answer -> {
@@ -95,14 +95,6 @@ public class BudgetControllerTest {
               .filter(role -> role.getId().equals(id))
               .findAny();
         });
-
-  }
-
-  @Before
-  public void setup() {
-    MockitoAnnotations.initMocks(this);
-    createdRoles = new LinkedList<>();
-    testUsers = new LinkedList<>();
   }
 
   @Test
@@ -116,8 +108,10 @@ public class BudgetControllerTest {
 
     RequestBuilder requestBuilder = MockMvcRequestBuilders
         .post("/api/budget")
+        .characterEncoding("utf-8")
         .header("authorization", user.getId().toString())
         .accept(MediaType.APPLICATION_JSON)
+        .contentType(MediaType.APPLICATION_JSON)
         .content(String.format("{\"id\": \"%s\"}", user.getId()));
 
     try {
