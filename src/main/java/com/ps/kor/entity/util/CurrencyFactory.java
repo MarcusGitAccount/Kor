08351815f52783804cfa59431d1ca62f6362c80d
Marcus Pop <marcuspopb@gmail.com>
2020-03-22 00:57:41 +0200

    Added javadoc.

--- src/main/java/com/ps/kor/entity/util/CurrencyFactory.java
+++ src/main/java/com/ps/kor/entity/util/CurrencyFactory.java
@@ -5,21 +5,33 @@ import java.util.Currency;
 import java.util.Set;
 import java.util.TreeSet;
 
-import lombok.extern.java.Log;
+import lombok.extern.log4j.Log4j2;
 
-@Log
+@Log4j2
 public class CurrencyFactory {
 
   private static final String defaultCurrencyCode = "RON";
 
   private static final Set<String> supportedCurrencyCodes = new TreeSet<>();
 
+  /**
+   * Init the set of supported currencies by the system.
+   */
   static {
     supportedCurrencyCodes.addAll(Arrays.asList(
        "RON", "EUR", "USD"
     ));
   }
 
+  /**
+   * Given the 3 letter representation for a currency
+   * return the afferent Java Currency implementation.
+   *
+   * @param code
+   * @return the Currency object for the given code
+   *         or the default Currency if the requested one
+   *         is not supported
+   */
   public static Currency getCurrency(String code) {
     if (!supportedCurrencyCodes.contains(code)) {
       log.info(String.format("Currency %s not supported by the system. Will default to %s.",
