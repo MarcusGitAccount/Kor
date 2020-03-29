f93decd5271a6e4eb5318aa6fb02ade4b86c74b5
Marcus Pop <marcuspopb@gmail.com>
2020-03-29 16:58:30 +0300

    Removed unused packages.

--- src/main/java/com/ps/kor/KorApplication.java
+++ src/main/java/com/ps/kor/KorApplication.java
@@ -3,8 +3,6 @@ package com.ps.kor;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
-import org.springframework.context.annotation.ComponentScan;
-import org.springframework.stereotype.Component;
 
 @SpringBootApplication(exclude = SecurityAutoConfiguration.class)
 public class KorApplication {
